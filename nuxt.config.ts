22f713c708b4804db53996b5671fba1a8c1c3b09
bocai <365191806@qq.com>
2023-11-30 21:54:45 +0800

    添加鼠标点击烟花特效，添加全局全屏背景图片

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,5 +18,6 @@ export default defineNuxtConfig({
   },
   css: [
     '~/assets/sass/Normalize.scss',
+    '~/assets/sass/myBackgroundImage.scss'
   ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,9 @@ const heandleclick = () => {
 
 <template>
 <div>
+    <div style="width: 100%;height: 100vh;">
+      asd
+    </div>
     <div class="common-layout">
     <el-container>
       <el-header style="width: 100%;height: 10vh;; background-color: antiquewhite;">
