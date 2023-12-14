b535b2ee2d38add874ea092ca2227817bdb69842
woaibocai <365191806@qq.com>
2023-12-14 19:10:05 +0800

    添加 登录 | 登出 | 获取用户信息 | 注册 | 接口

--- api/login.ts
+++ api/login.ts
@@ -1,14 +1,40 @@
 import { useFetch } from "nuxt/app";
-import { useMyFetch } from "~/composables/useMyFetch";
+// import { useMyFetch } from "~/composables/useMyFetch";
+const URL = "/api/blog/user/";
 type formData = {
   userName: string;
   password: string;
 };
+
 const Login = (data: formData) => {
-  return $fetch("http://localhost:16280/blog/user/login", {
+  return $fetch("http://localhost:16280/api/blog/user/login", {
+    method: "post",
+    body: data,
+  });
+};
+//登出
+type Token = {
+  token: string;
+  refresh_token: string;
+};
+const Logout = (data: Token) => {
+  return useFetch(URL + "logout", {
+    method: "post",
+    body: data,
+  });
+};
+//注册 register
+type RegisterData = {
+  userName: string;
+  password: string;
+  email: string;
+};
+const Register = (data: RegisterData) => {
+  console.log(data);
+  return useMyOtherFetch(URL + "register", {
     method: "post",
     body: data,
   });
 };
 
-export { Login };
+export { Login, Logout, Register };
