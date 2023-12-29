a0a191cc391047b00864db80edfac3825ed9a634
woaibocai <365191806@qq.com>
2023-12-30 05:32:13 +0800

    小细节 | 为了更好的SEO能用a标签就用a标签，不要考虑性能

---
+++ pages/article/[id].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+useHead({
+  title: "文章",
+  meta: [
+    {
+      name: "description",
+      content: "文章 : 这是一个充满传奇故事狗狗的互联网小窝",
+    },
+    { name: "keywords", content: "文章,群晖,IT,Java,emby" },
+  ],
+});
+</script>
+
+<template>
+  {{ $route.params.id }}
+</template>
+
+<style lang="scss" scoped></style>
