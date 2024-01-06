f86c2538375e007bb32bfdd00785641c5e907e4d
woaibocai <365191806@qq.com>
2024-01-06 16:13:22 +0800

    添加 GetArticlePageBytagUrl 接口 | 添加 /tag/[id].vue

---
+++ pages/tag/[id].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div style="display: flex; justify-content: center">
+    <div>
+      <AppMainMyAlert />
+      {{ route.params.id }}
+      <!-- <AppMainArticle /> -->
+      <AppButtom :my-class="'MyButtom'" />
+    </div>
+    <div style="margin-left: 30px">
+      <AppMainRightBlogInfo />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
