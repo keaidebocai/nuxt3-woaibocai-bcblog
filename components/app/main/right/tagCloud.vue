c59da5633380d66918f4d5c198e4a21d8d778f01
woaibocai <365191806@qq.com>
2024-01-07 23:07:36 +0800

    移动端

--- components/app/main/right/tagCloud.vue
+++ components/app/main/right/tagCloud.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- touchmove -->
   <div
     class="cloud"
     :style="{ width: width + 'px', height: height + 'px' }"
@@ -16,7 +17,7 @@
       }"
     >
       <a
-        :href="cloud.tagUrl"
+        :href="'/tag/' + cloud.tagUrl"
         :style="{ color: cloud.color, textDecoration: 'none' }"
       >
         {{ cloud.tagName }}
@@ -27,19 +28,6 @@
 
 <script lang="ts" setup>
 import { computed, onMounted, onUnmounted, reactive } from "vue";
-// const tags = [
-//   { name: "01. 人员招聘难", color: "#006666" },
-//   { name: "02. 人才复制难", color: "#009999" },
-//   { name: "03. 门店管理难", color: "#333333" },
-//   { name: "04. 客户引流难", color: "#660099" },
-//   { name: "05. 客户不稳定", color: "#990066" },
-//   { name: "06. 技术门槛高", color: "#CC0099" },
-//   { name: "07. 营销策划能", color: "#CC3300" },
-//   { name: "08. 行业内卷利", color: "#FF9900" },
-//   { name: "09. 产品严重", color: "#006633" },
-//   { name: "10. 技术项目" }, // color 可以不传，默认黑色
-// ];
-
 interface Tag {
   tagName: string;
   color?: string;
